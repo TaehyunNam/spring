0665abb0801435385e838ac587db6b23580b44bb
taehyunNam <nth2660@naver.com>
2021-12-08 10:08:39 +0900

    초기세팅

---
+++ spring/src/main/java/chap07/user/SchoolVo.java
@@ -0,0 +1,34 @@
+package chap07.user;
+
+public class SchoolVo {
+
+	private int schoolno;
+	private int userno;
+	private String school;
+	private String year;
+	
+	int getSchoolno() {
+		return schoolno;
+	}
+	void setSchoolno(int schoolno) {
+		this.schoolno = schoolno;
+	}
+	int getUserno() {
+		return userno;
+	}
+	void setUserno(int userno) {
+		this.userno = userno;
+	}
+	String getSchool() {
+		return school;
+	}
+	void setSchool(String school) {
+		this.school = school;
+	}
+	String getYear() {
+		return year;
+	}
+	void setYear(String year) {
+		this.year = year;
+	}
+}
